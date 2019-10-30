9aadab1ab39289dd033321f4c7690398267ccd9e
Ziio <calvinleung0@gmail.com>
2019-10-30 17:42:20 -0400

    components, images, and pages

---
+++ raimu/src/pages/Intro/Intro.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Container, Row} from '../../components/Grid';
+
+
+class intro extends Component {
+
+    render() {
+
+        return(
+
+        <Container>
+            <Row>
+                <div id='logo'></div>
+            </Row>
+        </Container>
+
+        )
+    }
+
+}
+
+export default intro;
